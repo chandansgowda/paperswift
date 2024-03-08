dbe58fd85d6e936fa1d8d506a73918c5135a319a
chandansgowda <chandansuresh007@gmail.com>
2024-03-08 23:54:22 +0530

    feat: auth controller (login)

---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'package:get/get.dart';
+import 'package:logger/logger.dart';
+import 'package:paperswift/utils/constants.dart';
+
+class AuthController extends GetxController {
+  late FlutterSecureStorage storage;
+  Logger log = Logger();
+  RxBool isLoading = false.obs;
+
+  @override
+  void onInit() {
+    storage = const FlutterSecureStorage();
+    super.onInit();
+  }
+
+  Future<void> login(String username, String email, String password) async {
+    try {
+      var url = Uri.parse('${baseUrl}login/');
+      var headers = {'accept': 'application/json', 'Content-Type': 'application/json'};
+      var body = json.encode({
+        'username': username,
+        'email': email,
+        'password': password,
+      });
+
+      isLoading.value = true;
+      var response = await http.post(url, headers: headers, body: body);
+
+      if (response.statusCode == 200) {
+        var jsonResponse = json.decode(response.body);
+        String token = jsonResponse['key'];
+
+        // Store token in secure-storage
+        await storage.write(key: "token", value: token);
+
+        log.i("Login successful. Stored Token: $token");
+      } else {
+        // Handle error responses here
+        log.w('Request failed with status: ${response.statusCode}.');
+      }
+    } catch (error) {
+      log.e('Error occurred while logging in: $error');
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+// API
+String baseUrl = "https://paperswiftsjcetest.pythonanywhere.com/auth/";
+
+// Theme
 const primaryColor = Color(0xFF2697FF);
 const secondaryColor = Color(0xFF2A2D3E);
 const bgColor = Color(0xFF212332);
 
+// Sizing
 const defaultPadding = 16.0;
